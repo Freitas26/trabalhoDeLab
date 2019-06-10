39a281f8015ee011ebcd9bfb5a98a3288e2a0fc8
Layla <laylacsa@msn.com>
2019-06-10 09:41:54 -0300

    Tirando as coisas inúteis que esquecemos

--- omp_integral.cpp
+++ omp_integral.cpp
@@ -4,15 +4,14 @@
 #include <omp.h>
 
 int main(){
-    int x = 0;
     float local_n = 1024;
-    float integral = x*x;
-    int i=0;
+    float integral = 0;
+    int i;
     float altura;
     float largura = (1/local_n);
 
     #pragma omp parallel for private(i) reduction(+:integral)
-    for(i=1; i<(int)local_n; i++){
+    for(i = 1; i<(int)local_n; i++){
         altura = (i/local_n)*(i/local_n); // calcula f(x) naquele ponto
         integral+= (altura*largura);
     }
