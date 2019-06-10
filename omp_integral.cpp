20b7c15279d6a840e11598122d120905c553376e
Layla <laylacsa@msn.com>
2019-06-10 10:14:09 -0300

    Corrigindo condições de corrida na função

--- omp_integral.cpp
+++ omp_integral.cpp
@@ -7,12 +7,11 @@ int main(){
     float local_n = 1024;
     float integral = 0;
     int i;
-    float altura;
     float largura = (1/local_n);
 
     #pragma omp parallel for private(i) reduction(+:integral)
     for(i = 1; i<(int)local_n; i++){
-        altura = (i/local_n)*(i/local_n); // calcula f(x) naquele ponto
+        float altura = (i/local_n)*(i/local_n); // calcula f(x) naquele ponto
         integral+= (altura*largura);
     }
 
